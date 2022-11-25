833a7b51902971c94e2f9faf60d5c73fa06143ea
aidar.bariev <aidarbariev@gmail.com>
2022-11-24 22:09:53 -0800

    Fix config generaiton

--- hive/h-run.sh
+++ hive/h-run.sh
@@ -5,5 +5,4 @@
 CUSTOM_LOG_BASEDIR=`dirname "$CUSTOM_LOG_BASENAME"`
 [[ ! -d $CUSTOM_LOG_BASEDIR ]] && mkdir -p $CUSTOM_LOG_BASEDIR
 
-cmd_line=$(cat tmfile)
-zelhash-opencl-miner $cmd_line 2>&1 | tee $CUSTOM_LOG_BASENAME.log
+zelhash-opencl-miner $(< $CUSTOM_CONFIG_FILENAME) 2>&1 | tee --append $MINER_LOG_BASENAME.log
